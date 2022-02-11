f6c408add96a6aaa3db48cf997ef2cf367ce8a1e
KarasuTatehi <45784893+KarasuTatehi@users.noreply.github.com>
2022-02-12 00:04:00 +0900

    material-uiを削除

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,10 +7,6 @@
 module.exports = {
   /* Your site config here */
   plugins: [
-    "gatsby-plugin-material-ui",
-    "gatsby-plugin-mui-emotion",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-top-layout",
     "gatsby-plugin-typegen",
   ],
 }
