139010acd7ea708190a9c7ed343cf1b7e20c3438
KarasuTatehi <45784893+KarasuTatehi@users.noreply.github.com>
2022-02-15 04:21:32 +0000

    loadable化

---
+++ src/components/loadable-index.tsx
@@ -0,0 +1,51 @@
+import styled from "@emotion/styled"
+import React, { useEffect, useRef, useState } from "react"
+import { peer } from "../utils/skyway"
+
+let localStream: MediaStream
+
+const App: React.VFC = () => {
+  const videoRef = useRef<HTMLVideoElement>(null)
+  const [peerId, setPeerId] = useState("")
+
+  useEffect(() => {
+    navigator.mediaDevices
+      .getUserMedia({
+        audio: false,
+        video: {
+          frameRate: 30,
+          height: 480,
+          width: 640,
+        },
+      })
+      .then(stream => {
+        localStream = stream
+        if (!videoRef.current) return
+        videoRef.current.srcObject = stream
+      })
+      .catch(console.error)
+
+    peer.on("open", setPeerId)
+
+    peer.on("call", conn => {
+      conn.answer(localStream, { videoCodec: "VP9" })
+    })
+  }, [])
+
+  return (
+    <>
+      <div>
+        <Video autoPlay muted playsInline ref={videoRef} />
+      </div>
+      <div>PeerID: {peerId}</div>
+    </>
+  )
+}
+
+const Video = styled("video")`
+  background-color: #000;
+  width: 640px;
+  height: 480px;
+`
+
+export default App
--- src/pages/call.tsx
+++ src/pages/call.tsx
@@ -1,43 +1,5 @@
-import styled from "@emotion/styled"
-import React, { useRef, useState } from "react"
-import { peer } from "../utils/skyway"
+import Loadable from "@loadable/component"
 
-type handleChangeType = React.ChangeEventHandler<HTMLInputElement>
-type handleCallType = React.MouseEventHandler<HTMLButtonElement>
-
-const App: React.VFC = () => {
-  const [peerId, setPeerId] = useState("")
-  const videoRef = useRef<HTMLVideoElement>(null)
-
-  const handleChange: handleChangeType = ev => {
-    setPeerId(ev.target.value)
-  }
-
-  const handleCall: handleCallType = () => {
-    const mediaConnection = peer.call(peerId)
-    mediaConnection.on("stream", stream => {
-      if (!videoRef.current) return
-      videoRef.current.srcObject = stream
-    })
-  }
-
-  return (
-    <>
-      <div>
-        <Video autoPlay muted playsInline ref={videoRef} />
-      </div>
-      <div>
-        <input onChange={handleChange} type="text" />
-        <button onClick={handleCall}>Call</button>
-      </div>
-    </>
-  )
-}
-
-const Video = styled("video")`
-  background-color: #000;
-  width: 640px;
-  height: 480px;
-`
+const App = Loadable(() => import("../components/loadable-call"))
 
 export default App
