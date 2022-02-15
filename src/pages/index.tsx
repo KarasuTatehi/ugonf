c89b0806c68d88be16f4031f131cbb8bbe9b0778
KarasuTatehi <45784893+KarasuTatehi@users.noreply.github.com>
2022-02-15 02:56:53 +0000

    送受信を分離

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,17 @@
 import styled from "@emotion/styled";
-import React, { useState, useRef, useEffect } from "react";
-import { MediaConnection } from "skyway-js";
-import peer from "../utils/skyway";
+import React, { useEffect, useRef, useState } from "react";
+import { peer } from "../utils/skyway";
 
 let localStream: MediaStream;
 
-const Home: React.VFC = () => {
-  const myRef = useRef<HTMLVideoElement>(null);
-  const theirRef = useRef<HTMLVideoElement>(null);
-  const [peerId, setPeerId] = useState<string>("");
-  const [theirId, setTheirId] = useState<string>("");
-  // const [localStream, setLocalStream] = useState(new MediaStream());
-  // const [remoteStream, setRemoteStream] = useState(new MediaStream());
+const App: React.VFC = () => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const [peerId, setPeerId] = useState("");
 
   useEffect(() => {
     navigator.mediaDevices
       .getUserMedia({
-        audio: true,
+        audio: false,
         video: {
           frameRate: 30,
           height: 480,
@@ -24,66 +19,40 @@ const Home: React.VFC = () => {
         },
       })
       .then((stream) => {
-        // setLocalStream(stream);
         localStream = stream;
-        if (!myRef.current) return;
-        myRef.current.srcObject = stream;
+        if (!videoRef.current) return;
+        videoRef.current.srcObject = stream;
       })
       .catch(console.error);
 
     peer.on("open", setPeerId);
-  }, []);
 
-  const handleCall = () => {
-    const mediaConnection = peer.call(theirId, localStream);
-    setEventListener(mediaConnection);
-  };
-
-  const setEventListener = (mediaConnection: MediaConnection) => {
-    mediaConnection.on("stream", (stream: MediaStream) => {
-      if (!theirRef.current) return;
-      theirRef.current.srcObject = stream;
+    peer.on("call", (conn) => {
+      conn.answer(localStream, { videoCodec: "VP9" });
     });
-  };
-
-  peer.on("call", (mediaConnection) => {
-    mediaConnection.answer(localStream);
-    setEventListener(mediaConnection);
-  });
-
-  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
-    setTheirId(event.target.value);
-  };
+  }, []);
 
   return (
     <>
-      <Video
-        autoPlay
-        muted
-        playsInline
-        ref={myRef}
-      />
-      <Video
-        autoPlay
-        muted
-        playsInline
-        ref={theirRef}
-      />
       <div>
-        PeerId: {peerId}
+        <Video
+          autoPlay
+          muted
+          playsInline
+          ref={videoRef}
+        />
       </div>
       <div>
-        <input onChange={handleChange} type="text" />
-        <button onClick={handleCall}>Send</button>
+        PeerID: {peerId}
       </div>
     </>
   );
-}
-
-export default Home;
+};
 
 const Video = styled("video")`
   background-color: #000;
   width: 640px;
   height: 480px;
 `;
+
+export default App;
