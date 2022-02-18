44d57eab1f387604d65e051c34d70a7fefc74c58
KarasuTatehi <45784893+KarasuTatehi@users.noreply.github.com>
2022-02-18 03:22:21 +0000

    URLクエリによる発信処理とGBフルスクリーン表示を実装

--- src/components/loadable/send.tsx
+++ src/components/loadable/send.tsx
@@ -1,45 +1,39 @@
 import styled from "@emotion/styled"
-import React, { useState, useRef } from "react"
+import { useLocation } from "@reach/router"
+import queryString from "query-string"
+import React, { useState, useRef, useEffect } from "react"
 import { peer } from "../../utils/skyway"
-
-type handleChangeType = React.ChangeEventHandler<HTMLInputElement>
-type handleCallType = React.MouseEventHandler<HTMLButtonElement>
 
 const App: React.VFC = () => {
   const [peerId, setPeerId] = useState("")
   const videoRef = useRef<HTMLVideoElement>(null)
+  const search = useLocation().search
+  const query = queryString.parse(search)
 
-  const handleChange: handleChangeType = ev => {
-    setPeerId(ev.target.value)
-  }
-
-  const handleCall: handleCallType = () => {
-    if (typeof window !== "undefined") {
-      const mediaConnection = peer.call(peerId)
+  useEffect(() => {
+    setTimeout(() => {
+      if (typeof query["peerId"] !== "string") return
+      const mediaConnection = peer.call(query["peerId"])
       mediaConnection.on("stream", stream => {
         if (!videoRef.current) return
         videoRef.current.srcObject = stream
       })
-    }
-  }
+    }, 3000)
+  }, [])
 
   return (
     <>
       <div>
         <Video autoPlay muted playsInline ref={videoRef} />
       </div>
-      <div>
-        <input onChange={handleChange} type="text" />
-        <button onClick={handleCall}>Call</button>
-      </div>
     </>
   )
 }
 
 const Video = styled("video")`
-  background-color: #000;
-  width: 640px;
-  height: 480px;
+  background-color: #0f0;
+  width: 100vw;
+  height: 100vh;
 `
 
 export default App
