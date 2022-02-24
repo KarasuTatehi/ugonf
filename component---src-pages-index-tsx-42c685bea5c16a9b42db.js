0151848a1055a94e43415024a01edcad14f1a838
KarasuTatehi <45784893+KarasuTatehi@users.noreply.github.com>
2022-02-24 22:12:19 +0900

    Updates

--- component---src-pages-index-tsx-42c685bea5c16a9b42db.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkugonf=self.webpackChunkugonf||[]).push([[691],{6738:function(e,n,u){u.r(n);var f=(0,u(5515).ZP)((function(){return Promise.all([u.e(800),u.e(351),u.e(876)]).then(u.bind(u,7876))}));n.default=f}}]);
-//# sourceMappingURL=component---src-pages-index-tsx-42c685bea5c16a9b42db.js.map
--- component---src-pages-send-tsx-70accfac5ea7cabbaf4f.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkugonf=self.webpackChunkugonf||[]).push([[586],{5669:function(e,n,u){u.r(n);var f=(0,u(5515).ZP)((function(){return Promise.all([u.e(800),u.e(351),u.e(378)]).then(u.bind(u,5378))}));n.default=f}}]);
-//# sourceMappingURL=component---src-pages-send-tsx-70accfac5ea7cabbaf4f.js.map
