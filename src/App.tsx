b8907240e240d4822125597f527c4e634789b705
KarasuTatehi <45784893+KarasuTatehi@users.noreply.github.com>
2022-03-04 17:06:01 +0900

    追加: react-helmet

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Helmet } from "react-helmet"
 import Home from "./pages/Home"
 import Receiver from "./pages/Receiver"
 import Sender from "./pages/Sender"
@@ -19,14 +20,19 @@ const App: React.VFC = () => {
   }, [])
 
   return (
-    <BrowserRouter basename={baseName}>
-      <Routes>
-        <Route element={<Home />} index />
-        <Route element={<Receiver />} path="/receiver" />
-        <Route element={<Receiver />} path="/receiver/:remotePeerId" />
-        <Route element={<Sender />} path="/sender" />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <Helmet>
+        <title>{`${process.env.REACT_APP_PACKAGE_NAME} ${process.env.REACT_APP_PACKAGE_VERSION}`}</title>
+      </Helmet>
+      <BrowserRouter basename={baseName}>
+        <Routes>
+          <Route element={<Home />} index />
+          <Route element={<Receiver />} path="/receiver" />
+          <Route element={<Receiver />} path="/receiver/:remotePeerId" />
+          <Route element={<Sender />} path="/sender" />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
